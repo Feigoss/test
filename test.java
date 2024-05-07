ad16e14dfc9b5412a1608ef5b713d4bbf8147c3a
org.codesafe1 <org.codesafe1@jd.com>
2024-05-07 17:58:46 +0800

    1

--- test.java
+++ test.java
@@ -8,8 +8,11 @@ remoteServiceCaller
 
 isFullyAuthenticated
 isFullyAuthenticated
-forward:
-.shouldFilterAllDispatcherTypes(true)
+forward:shortName
+yn
+"isLOC"
+.shouldFilterAll
+"isLOC"DispatcherTypes(true)
 包含com.jd.union.passport.interceptor 即可GoodsTypeDiscerner
 
 (包含FormsAuthenticationTicket || 包含com.jd.passport.inteceptor.mvc.SpringMvcInterceptor || 包含com.jd.passport.inteceptor.JdLoginInteceptor) && (不包含remoteServiceCaller || 包含name="needRemote" value="false")
