e1aafc97f3338f120f9a0cef6f55349f37f775be
org.codesafe1 <org.codesafe1@jd.com>
2024-05-07 18:00:27 +0800

    1

--- test.java
+++ test.java
@@ -12,7 +12,7 @@ forward:shortName
 yn
 "isLOC"
 .shouldFilterAll
-"isLOC"DispatcherTypes(true)
+" isLOC   "DispatcherTypes(true)
 包含com.jd.union.passport.interceptor 即可GoodsTypeDiscerner
 
 (包含FormsAuthenticationTicket || 包含com.jd.passport.inteceptor.mvc.SpringMvcInterceptor || 包含com.jd.passport.inteceptor.JdLoginInteceptor) && (不包含remoteServiceCaller || 包含name="needRemote" value="false")
