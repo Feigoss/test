ec1572cf425a5e6681d789c28fcb7839ac6bd481
org.codesafe1 <org.codesafe1@jd.com>
2024-05-17 14:27:51 +0800

    1

--- test.java
+++ test.java
@@ -16,3 +16,7 @@ yn
 包含com.jd.union.passport.interceptor 即可GoodsTypeDiscerner
 
 (包含FormsAuthenticationTicket || 包含com.jd.passport.inteceptor.mvc.SpringMvcInterceptor || 包含com.jd.passport.inteceptor.JdLoginInteceptor) && (不包含remoteServiceCaller || 包含name="needRemote" value="false")
+
+
+
+password/openapi
